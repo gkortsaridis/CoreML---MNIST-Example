1142df302c3074ed5b32f53f1f5c417e8e4841ed
George Kortsaridis <gkortsaridis@gmail.com>
2019-06-19 21:37:46 +0100

    Initial Commit

---
+++ CoreML-MNIST/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreML-MNIST
+//
+//  Created by George Kortsaridis on 19/06/2019.
+//  Copyright © 2019 Bastionware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
